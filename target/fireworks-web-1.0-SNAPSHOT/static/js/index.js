35fc35a0e2e0d6172eac3416c085e283b015deca
chenjiayang <chenjiayang@meituan.com>
2018-03-21 17:18:23 +0800

    完成fireworks前端

---
+++ target/fireworks-web-1.0-SNAPSHOT/static/js/index.js
@@ -0,0 +1,17 @@
+var app = angular.module('fireworks', []);
+app.controller('FireworksIndexController', ['$scope', '$http', '$compile', function ($scope, $http, $compile) {
+    $scope.introduction = "从来不求时间为我搁浅...";
+    $scope.recentBlog = [
+        '每个人都有一个死角， 自己走不出来，别人也闯不进去。',
+        '我把最深沉的秘密放在那里。',
+        '你不懂我，我不怪你。'
+    ];
+    $scope.tagList = ['日志','Java','Android','跨年','数据库'];
+    $scope.titleList = [
+        '每个人都有一个死角， 自己走不出来，别人也闯不进去。',
+        '我把最深沉的秘密放在那里。',
+        '你不懂我，我不怪你。'
+    ];
+
+}
+]);
